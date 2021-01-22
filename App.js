38455cacaa36690f67a6a97a2133c1cb3a3a2fb1
Preston Wallace <wallace.preston@gmail.com>
2021-01-21 23:09:01 -0800

    feat(POC): Dog List fetching and displaying via button, with navigation to specific dog

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation, dogs, fetchDogs }) => {
+  return (<>
+    {
+      dogs.length
+        ? dogs.map(dog => <View key={dog.id}>
+          <Button
+            title={dog.name}
+            onPress={() =>
+              navigation.navigate('Profile', { dog })
+            }
+          />
+        </View>)
+        : <Button title="Show Dogs" onPress={() => fetchDogs()}></Button> 
+    }
+  </>
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  const {dog} = route.params;
+  return <>
+    <Text>This is {dog.name}'s profile</Text>
+    <Text>Email: {dog.email}</Text>
+    <Text>{dog.isCute && 'Cute Dog'}</Text>
+  </>;
+};
 
 export default function App() {
+  const [dogs, setDogs] = useState([]);
+
+  const fetchDogs = async () => {
+    const resp = await fetch('https://prestons-dog-api.netlify.app/all.json');
+    const data = await resp.json();
+    setDogs(data);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+        <Stack.Screen name="Home" options={{ title: 'Dog List' }}>
+          {props => <HomeScreen {...props} dogs={dogs} fetchDogs={fetchDogs}/>}
+        </Stack.Screen>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+
       <StatusBar style="auto" />
-    </View>
+
+    </NavigationContainer>
+
   );
 }
 
